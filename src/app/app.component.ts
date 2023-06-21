e31d199adc51e6b520c61876e588d3704e56e8d1
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-06-21 14:55:23 +0200

    clean workspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'poc-cdk-multi-drag-drop';
-}
+export class AppComponent {}
